63172ab264e1e1a2e549f4003c2f00a0383fe579
FaustianDealer134 <39071262+FaustianDealer134@users.noreply.github.com>
2018-05-08 20:33:40 -0300

    Add files via upload

---
+++ DontLeave.asm
@@ -0,0 +1,18 @@
+COD	SEGMENT
+	ASSUME CS:COD, DS:COD, ES:COD, SS:COD
+	ORG 0x100h
+MAIN PROC NEAR
+	mov ax,0h
+	mov bx,0h
+	mov cx,0h
+	mov dx,0h
+memor:
+start:
+input:
+move:
+inter1:
+inter2:
+
+MAIN ENDP
+COD ENDS
+	END MAIN
