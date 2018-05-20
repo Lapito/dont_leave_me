96ed3ce6801fef145c576b081b851c5fb6048e1f
FaustianDealer134 <39071262+FaustianDealer134@users.noreply.github.com>
2018-05-20 17:58:40 -0300

    Update DontLeave.asm

--- DontLeave.asm
+++ DontLeave.asm
@@ -1,19 +1,26 @@
-COD	SEGMENT
-	ASSUME CS:COD, DS:COD, ES:COD, SS:COD
-	ORG 0x100h
+COD SEGMENT
+    ASSUME CS:COD, DS:COD, ES:COD, SS:COD
+    ORG 0x100h
 MAIN PROC NEAR
-	mov ax,0h
-	mov bx,0h
-	mov cx,0h
-	mov dx,0h
+    mov ax,0h
+    mov bx,0h
+    mov cx,0h
+    mov dx,0h
 memor:
+    ;setup
 start:
-loop:
-input:
+    ;start screen
+beggining:
+    ;start of game
+waiting:
+    ;main loop
 move:
+    ;movement
 inter1:
+    ;I don't feel so good
 inter2:
+    ;why is gamora
 
 MAIN ENDP
 COD ENDS
-	END MAIN
+    END MAIN
