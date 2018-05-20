d1ad19d07c7bc781bb18a91e60bae487f7dc5ec6
FaustianDealer134 <39071262+FaustianDealer134@users.noreply.github.com>
2018-05-20 19:23:38 -0300

    Update DontLeave.asm

--- DontLeave.asm
+++ DontLeave.asm
@@ -6,20 +6,58 @@ MAIN PROC NEAR
     mov bx,0h
     mov cx,0h
     mov dx,0h
-memor:
-    ;setup
+setup:
+	;start setup from 500
+    ;events
+	;equipped item
+	;strings
+	;locations
+	;event flags
+	;GUI
 start:
-    ;start screen
+    ;print title
+    ;print, Press any buttom to start
+    mov ax,00h
+    mov ah,0bh
+    int 21h
+    cmp la, 00h
+    jnz beggining
 beggining:
     ;start of game
+	;initial exposition
 waiting:
     ;main loop
+	;render graphics
+	;wait for input, up, down, left, right, interact 
 move:
     ;movement
+	;check input
+	;mov if possible
+	;check for events
+	;refresh screen
+	;save location
+interaction:
+	;interact with environment
+	;check for interaction flags
+	;jump for interaction
 inter1:
-    ;I don't feel so good
+    ;interaction 1
+	;description
+	;check item
 inter2:
-    ;why is gamora
+    ;interaction 2
+	;description
+	;present choice
+render1:
+	;graphic function room 1
+render2:
+	;graphic function room 2
+render3:
+	;graphic function room 3
+ending:
+	;return to OS
+memor:
+	;start of memory allocation
 
 MAIN ENDP
 COD ENDS
